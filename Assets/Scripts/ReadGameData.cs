8c50c6c4b80deaf46ac81d95ebb96c8f4050964e
ihartog <26035@ma-web.nl>
2019-11-20 12:22:06 +0100

    json files
    Json aan het uittesten in unity

---
+++ Assets/Scripts/ReadGameData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using UnityEngine;
+
+public class ReadGameData : MonoBehaviour
+{
+    public GameObject character;
+    public GameObject hairType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public string write()
+    {
+        return JsonUtility.ToJson(character, hairType);
+    }    
+}
