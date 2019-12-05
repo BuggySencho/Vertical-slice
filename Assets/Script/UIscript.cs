1d54f9999b207638aadcf66229629b4b86267456
HBlueRainDrop <43329986+HBlueRainDrop@users.noreply.github.com>
2019-12-05 10:05:47 +0100

    Door Herbert

---
+++ Assets/Script/UIscript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIscript : MonoBehaviour
+{
+    [SerializeField] private Canvas canvas;
+    [SerializeField]private GameObject scrollbar;
+    [SerializeField] private float number;
+    [SerializeField] private Button Up;
+    [SerializeField] private Button Down;
+
+    private void Start()
+    {
+        Up.onClick.AddListener(GoUp);
+        Down.onClick.AddListener(GoDown);
+    }
+    private void Update()
+    {
+        RectTransform objectRectTransform = gameObject.GetComponent<RectTransform>();
+        Debug.Log("width: " + objectRectTransform.rect.width + ", height: " + objectRectTransform.rect.height);
+        number = objectRectTransform.rect.height / 5.7f;
+//        if (Input.GetKeyDown(KeyCode.UpArrow))
+//        {
+//            scrollbar.transform.position -= new Vector3(0, number, 0);
+//        }
+//        else if (Input.GetKeyDown(KeyCode.DownArrow))
+//        {
+//            scrollbar.transform.position += new Vector3(0, number, 0);
+//        }
+    }
+
+    void GoUp()
+    {
+        scrollbar.transform.position -= new Vector3(0, number, 0);
+    }
+    void GoDown()
+    {
+        scrollbar.transform.position += new Vector3(0, number, 0);
+    }
+}
