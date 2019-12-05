d320a853a6338034bd36b1e43ccf292461c21338
ihartog <26035@ma-web.nl>
2019-12-05 12:09:07 +0100

    Json solved
    Json uitdaging opgelost met jsonUtility en system.IO

---
+++ Assets/SaveGameData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGameData : ReadJson
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SaveData();
+        }
+        
+    }
+
+    void SaveData()
+    {
+        string contents = JsonUtility.ToJson(gameData, true);
+        System.IO.File.WriteAllText(path, contents);
+        Debug.Log("file saved");
+    }
+}
